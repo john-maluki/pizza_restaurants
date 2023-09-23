6d7f6e88f389624c9b750ce57babd0a8d28ab65d
john <johnmaluki58@gmail.com>
2023-09-24 01:18:10 +0300

    Configure every extension inside __init__

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from app.models import db
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "jdbwejbfewub=wehdweh64wdhwvh-bhcb"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///restaurant_pizza.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
