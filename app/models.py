9046cc4bb792015daded8938d72887e898e05ce4
john <johnmaluki58@gmail.com>
2023-09-23 19:45:28 +0300

    Add price constraint

--- app/models.py
+++ app/models.py
@@ -1,10 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
 
 
-class Pizza(db.Model):
+class Pizza(db.Model, SerializerMixin):
     __tablename__ = "pizzas"
+
+    serialize_rules = ("-restaurant_pizzas.pizza",)
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
@@ -18,8 +21,10 @@ class Pizza(db.Model):
         return f"<Pizza {self.name}>"
 
 
-class Restaurant(db.Model):
+class Restaurant(db.Model, SerializerMixin):
     __tablename__ = "restaurants"
+
+    serialize_rules = ("-restaurant_pizzas.restaurant",)
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True)
@@ -31,25 +36,23 @@ class Restaurant(db.Model):
         return f"<Restaurant {self.name} {self.address}>"
 
 
-class RestaurantPizza(db.Model):
-    __tablename__ = "restaurant_pizza"
+class RestaurantPizza(db.Model, SerializerMixin):
+    __tablename__ = "restaurant_pizzas"
+
+    serialize_rules = ("-pizza.restaurant_pizzas",)
+    serialize_rules = ("-restaurant.restaurant_pizzas",)
 
     id = db.Column(db.Integer, primary_key=True)
-    price = db.Column(db.Integer)
+    price = db.Column(
+        db.Integer,
+        db.CheckConstraint(
+            "price >=1 AND price <=30", name="Price value is not with range 1 and 30"
+        ),
+    )
     pizza_id = db.Column(db.Integer, db.ForeignKey("pizzas.id"))
     restaurant_id = db.Column(db.Integer, db.ForeignKey("restaurants.id"))
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add CheckConstraint to enforce min and max values for price
-    min_price = 1
-    max_price = 30
-
-    __table_args__ = (
-        db.CheckConstraint(
-            f"{price} >= {min_price} AND {price} <= {max}", name="check_value_range"
-        ),
-    )
-
     def __repr__(self):
-        return f"<RestaurantPizza {self.price} Restaurant:{self.restaurant} Pizza: {self.pizza}>"
+        return f"<RestaurantPizza {self.price}>"
