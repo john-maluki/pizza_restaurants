73ceb581438607451d08312b51267772185dd740
john <johnmaluki58@gmail.com>
2023-09-24 00:07:07 +0300

    Change table relationship

--- app/models.py
+++ app/models.py
@@ -1,13 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
 
 
-class Pizza(db.Model, SerializerMixin):
+class Pizza(db.Model):
     __tablename__ = "pizzas"
-
-    serialize_rules = ("-restaurant_pizzas.pizza",)
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
@@ -15,32 +12,31 @@ class Pizza(db.Model, SerializerMixin):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    restaurant_pizzas = db.relationship("RestaurantPizza", backref="pizza")
+    restaurants = db.relationship(
+        "Restaurant", secondary="restaurant_pizzas", back_populates="pizzas"
+    )
 
     def __repr__(self):
         return f"<Pizza {self.name}>"
 
 
-class Restaurant(db.Model, SerializerMixin):
+class Restaurant(db.Model):
     __tablename__ = "restaurants"
-
-    serialize_rules = ("-restaurant_pizzas.restaurant",)
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True)
     address = db.Column(db.String)
 
-    restaurant_pizzas = db.relationship("RestaurantPizza", backref="restaurant")
+    pizzas = db.relationship(
+        "Pizza", secondary="restaurant_pizzas", back_populates="restaurants"
+    )
 
     def __repr__(self):
         return f"<Restaurant {self.name} {self.address}>"
 
 
-class RestaurantPizza(db.Model, SerializerMixin):
+class RestaurantPizza(db.Model):
     __tablename__ = "restaurant_pizzas"
-
-    serialize_rules = ("-pizza.restaurant_pizzas",)
-    serialize_rules = ("-restaurant.restaurant_pizzas",)
 
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(
